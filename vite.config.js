6d9e9305c13349c9145d4b81a2f74657ce2f428e
gear_kinu <techgit1024@gmail.com>
2025-05-04 15:55:13 +0900

    .envからsupabaseKeyなどの読み込み（Vite）

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import env from "vite-plugin-env-compatible";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), env({ prefix: "VITE", mountedPath: "process.env"})],
 })
